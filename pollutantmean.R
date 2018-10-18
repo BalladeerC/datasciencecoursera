5cb769a887bec2f6cee83285ce280f3979ac97c3
Balladeer <jscromie@gmail.com>
2018-10-18 19:37:38 +0100

    Deleted files successfully, programming update done

--- pollutantmean.R
+++
@@ -1,8 +0,0 @@
-pollutantmean <- function(directory,pollutant,id=1:332){
-  files_list <- list.files("specdata", full.names=TRUE)
-  frame <- data.frame()
-  for(j in id){
-    frame <- rbind(frame,read.csv(files_list[j]))
-  }
-  mean(frame[,pollutant],na.rm=TRUE)
-}
